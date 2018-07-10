93b64914c7990165f04cf4359ce8ebb4b7e9689d
HernandoYu <yhn1995pku@163.com>
2018-07-10 18:59:57 +0800

    all players

--- HupuSpider/spiders/HupuPlayerSpider.py
+++ HupuSpider/spiders/HupuPlayerSpider.py
@@ -2,6 +2,10 @@
 
 import scrapy
 from HupuSpider.items import HupuspiderItem
+import os
+
+columns_ch = [u'姓名', u'位置', u'身高', u'体重', u'生日', u'球队',
+              u'学校', u'选秀', u'国籍', u'本赛季薪金', u'合同']
 
 class HupuPlayerSpider(scrapy.Spider):
     name = 'hupu'
@@ -13,46 +17,47 @@ class HupuPlayerSpider(scrapy.Spider):
         #self.log('parsing: %s' % response.url)
 
         teams = response.css('a.a_teamlink')
-        a = 0
+
         for team in teams:
             team_link = team.css('::attr(href)').extract_first()
             team_player_url = team_link.replace('team', 'player')
             if team_player_url is not None:
                 yield scrapy.Request(team_player_url, callback=self.parse_team)
-            a += 1
-            if a > 0:
-                break
+
 
 
     def parse_team(self, response):
         #self.log('parsing: %s' % response.url)
 
         players =  response.css('td.left b a')
-        a = 0
+
         for player in players:
             player_link = player.css('::attr(href)').extract_first()
             if player_link is not None:
                 yield scrapy.Request(player_link, callback=self.parse_player)
-            a += 1
-            if a > 1:
-                break
 
 
     def parse_player(self, response):
         #self.log('parsing: %s' % response.url)
 
-        f = open('playerinfo.csv', 'a')
+        if not os.path.exists('playerinfo.csv'):
+            f = open('playerinfo.csv', 'a')
+            f.write(','.join(columns_ch).encode('utf-8'))
+        else:
+            f = open('playerinfo.csv', 'a')
 
         player_info = response.css('div.team_data')
 
         name = player_info.css('h2::text').extract_first()
+        self.log(name)
         f.write(name.encode('utf-8'))
 
         for p in player_info.css('div.font p'):
             info = p.css('::text').extract_first()
             if p.css('a'):
                 info = info + p.css('a::text').extract_first()
-            self.log(info)
+            info = info.split(u'：')[-1]
+            #self.log(info)
             f.write(',')
             f.write(info.encode('utf-8'))
         f.close()
